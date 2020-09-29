0d725bad2fa502048f24829aa538050aa2b1110c
Alexander Vetlugin <vetlugins@gmail.com>
2020-09-30 00:06:50 +0300

    init

--- assets/js/vgsidebars.js
+++ assets/js/vgsidebars.js
@@ -1,3 +1,37 @@
-function great(input) {
-	return input ?? 'Hello world';
-}
+(function( $ ){
+	
+	const NAME = 'vg-sidebar';
+	const DATA_KEY = 'vg.sidebar';
+	
+	const CLASS_SIDEBAR_SHOW = 'show';
+	
+	let params = {
+		target: '',
+		placement: 'right',
+		ajax: false
+	};
+	
+	let methods = {
+		show : function(options) {
+			let settings = $.extend(params, options);
+			
+			console.log(settings);
+			
+			return this;
+		},
+		hide : function( ) {
+
+		}
+	};
+	
+	$.fn.vgSidebar = (method) => {
+		if ( methods[method] ) {
+			return methods[method].apply(this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.show.apply( this, arguments );
+		} else {
+			$.error( 'Метод с именем ' +  method + ' не существует ' );
+		}
+	}
+	
+})( jQuery );
--- dist/js/vgsidebars.js
+++ dist/js/vgsidebars.js
@@ -1,5 +1,30 @@
 "use strict";
 
-function great(input) {
-  return input !== null && input !== void 0 ? input : 'Hello world';
-}
+(function ($) {
+  var NAME = 'vg-sidebar';
+  var DATA_KEY = 'vg.sidebar';
+  var CLASS_SIDEBAR_SHOW = 'show';
+  var params = {
+    target: '',
+    placement: 'right',
+    ajax: false
+  };
+  var methods = {
+    show: function show(options) {
+      var settings = $.extend(params, options);
+      console.log(settings);
+      return this;
+    },
+    hide: function hide() {}
+  };
+
+  $.fn.vgSidebar = method => {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method) {
+      return methods.show.apply(this, arguments);
+    } else {
+      $.error('Метод с именем ' + method + ' не существует ');
+    }
+  };
+})(jQuery);
