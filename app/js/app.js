2b10f5ed5936f6dda74f2d9d7a23a676f73770b5
Alexander Vetlugin <vetlugins@gmail.com>
2026-06-11 22:19:06 +0300

    1.3.7

--- app/js/app.js
+++ app/js/app.js
@@ -12,12 +12,17 @@ const EVENT_KEY_SHOW = 'vg.sidebar.show';
 const EVENT_KEY_SHOWN = 'vg.sidebar.shown';
 const EVENT_KEY_LOADED = 'vg.sidebar.loaded';
 
-let	_cross = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"' +
+const _cross = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"' +
 		'\t viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">' +
 		'<path d="M89.7,10.3L89.7,10.3c-1-1-2.6-1-3.5,0L50,46.5L13.9,10.3c-1-1-2.6-1-3.5,0l0,0c-1,1-1,2.6,0,3.5L46.5,50L10.3,86.1' +
 		'\tc-1,1-1,2.6,0,3.5h0c1,1,2.6,1,3.5,0L50,53.5l36.1,36.1c1,1,2.6,1,3.5,0l0,0c1-1,1-2.6,0-3.5L53.5,50l36.1-36.1' +
 		'\tC90.6,12.9,90.6,11.3,89.7,10.3z"/>' +
 		'</svg>';
+
+const getScrollbarWidth = () => {
+	const documentWidth = document.documentElement.clientWidth
+	return Math.abs(window.innerWidth - documentWidth)
+};
 
 class VGSidebar extends BaseModule{
 	constructor(element, arg = {}) {
@@ -33,27 +38,35 @@ class VGSidebar extends BaseModule{
 			}
 		};
 		this.element = element;
+		this._onKeyUp = null;
 
-		let mParams = mergeDeepObject(arg, this.paramsDefault);
+		let mParams = mergeDeepObject(this.paramsDefault, arg);
 		if (isObject(mParams) && !isEmptyObj(mParams)) {
 			for (const datum in mParams) {
 				let value = normalizeData(mParams[datum]);
 
-				if (datum !== 'params') {
-					if (!(datum in this.paramsDefault)) {
-						let p = datum.split('-');
-
-						if (p[1] in this.paramsDefault[p[0]]) {
-							this.paramsDefault[p[0]][p[1]] = value;
-						}
-
-						delete mParams[datum];
-					} else {
-						mParams[datum] = value;
+				if (datum === 'params') {
+					if (isObject(value) && !isEmptyObj(value)) {
+						mParams = mergeDeepObject(mParams, value)
 					}
-				} else {
-					mParams = mergeDeepObject(mParams, value)
+
 					delete mParams[datum];
+					continue;
+				}
+
+				if (!(datum in this.paramsDefault)) {
+					let [group, key] = datum.split('-');
+
+					if (group && key && isObject(this.paramsDefault[group]) && key in this.paramsDefault[group]) {
+						if (!isObject(mParams[group])) {
+							mParams[group] = {};
+						}
+						mParams[group][key] = value;
+					}
+
+					delete mParams[datum];
+				} else {
+					mParams[datum] = value;
 				}
 			}
 		}
@@ -80,12 +93,13 @@ class VGSidebar extends BaseModule{
 
 	show() {
 		const _this = this;
+		if (_this._isShown()) return;
 
 		eventHandler.on(_this.element, EVENT_KEY_SHOW);
 
 		_this._backdrop();
-		_this._overflow();
 		_this.element.classList.add('show');
+		_this._overflow();
 
 		setTimeout(() => {
 			if (_this.params.ajax.route && _this.params.ajax.target) _this._route();
@@ -97,12 +111,14 @@ class VGSidebar extends BaseModule{
 
 	hide() {
 		const _this = this;
+		if (!_this._isShown()) return;
 
 		eventHandler.on(_this.element, EVENT_KEY_HIDE);
 
 		_this._backdrop();
-		_this._overflow();
 		_this.element.classList.remove('show');
+		_this._overflow();
+		_this._removeEventListener();
 
 		setTimeout(() => {
 			eventHandler.on(_this.element, EVENT_KEY_HIDDEN);
@@ -127,7 +143,8 @@ class VGSidebar extends BaseModule{
 				delete arg['target'];
 				delete arg['toggle'];
 
-				let sidebar = new VGSidebar(target, arg);
+				let sidebar = VGSidebar.getInstance(target);
+				if (!sidebar) sidebar = new VGSidebar(target, arg);
 				sidebar.toggle();
 			}
 
@@ -162,17 +179,12 @@ class VGSidebar extends BaseModule{
 			return;
 		}
 
-		if (!this._isShown) {
+		if (!this._isShown()) {
 			document.body.style.overflow = '';
 			document.body.style.paddingRight = '';
 		} else {
-			document.body.style.paddingRight = getWidth() + 'px';
+			document.body.style.paddingRight = getScrollbarWidth() + 'px';
 			document.body.style.overflow = 'hidden';
-		}
-
-		function getWidth() {
-			const documentWidth = document.documentElement.clientWidth
-			return Math.abs(window.innerWidth - documentWidth)
 		}
 	}
 
@@ -188,12 +200,13 @@ class VGSidebar extends BaseModule{
 			}
 		}
 
-		[...document.querySelectorAll('[data-vg-dismiss="sidebar"]')].forEach(function (cross) {
+		[..._this.element.querySelectorAll('[data-vg-dismiss="sidebar"]')].forEach(function (cross) {
 			cross.onclick = function () {
 				let target = cross.dataset.vgTarget || cross.closest('.vg-sidebar') || null;
 
 				if (target) {
-					VGSidebar.getInstance(target).hide();
+					let sidebar = VGSidebar.getInstance(target);
+					if (sidebar) sidebar.hide();
 				}
 
 				return false;
@@ -201,13 +214,22 @@ class VGSidebar extends BaseModule{
 		});
 
 		if (_this.params.keyboard) {
-			document.onkeyup = function (e) {
-				if (e.key === "Escape") {
-					_this.hide();
-				}
+			if (!_this._onKeyUp) {
+				_this._onKeyUp = function (e) {
+					if (e.key === "Escape") {
+						_this.hide();
+					}
+				};
+			}
 
-				return false;
-			};
+			document.removeEventListener('keyup', _this._onKeyUp);
+			document.addEventListener('keyup', _this._onKeyUp);
+		}
+	}
+
+	_removeEventListener() {
+		if (this._onKeyUp) {
+			document.removeEventListener('keyup', this._onKeyUp);
 		}
 	}
 
@@ -219,8 +241,10 @@ class VGSidebar extends BaseModule{
 			let request = new XMLHttpRequest();
 			request.open("get", _this.params.ajax.route, true);
 			request.onload = function () {
-				setData(request.responseText);
-				eventHandler.on(_this.element, EVENT_KEY_LOADED);
+				if (request.status >= 200 && request.status < 300) {
+					setData(request.responseText);
+					eventHandler.on(_this.element, EVENT_KEY_LOADED);
+				}
 			};
 			request.send();
 		}
